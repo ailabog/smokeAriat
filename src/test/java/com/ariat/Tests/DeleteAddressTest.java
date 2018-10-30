bbdf0e3584a6dd4c5e7a70b834e0854047dd0177
Aila Bogasieru <aila.bogasieru@ariat.com>
2018-10-30 20:44:57 +0200

    implement tests for delete address

---
+++ src/test/java/com/ariat/Tests/DeleteAddressTest.java
@@ -0,0 +1,98 @@
+package com.ariat.Tests;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import com.ariat.Enums.Environments;
+import com.ariat.Pages.AddAddressesPage;
+import com.ariat.Pages.AddressesPage;
+import com.ariat.Pages.HomePage;
+import com.ariat.Pages.LogoutPage;
+import com.ariat.Pages.MyAccountPage;
+import com.ariat.Pages.SignInPage;
+import com.ariat.Utils.GenerateRandomDataUtils;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+
+/**
+ * 
+ * @author aila.bogasieru@ariat.com
+ *
+ */
+
+public class DeleteAddressTest extends BaseTest {
+
+	private HomePage homePage;
+	private SignInPage signInPage;
+	private MyAccountPage myAccountPage;
+	private AddAddressesPage addAddressPage;
+	private AddressesPage addressesPage;
+	private LogoutPage logoutPage;
+	private Environments environment;
+
+	public static final String ADDRESS = GenerateRandomDataUtils.generateRandomString(5);
+	public static final String CITY = GenerateRandomDataUtils.generateRandomString(5);
+	public static final String POST_CODE = GenerateRandomDataUtils.generateRandomNumber(5);
+	public static final String PHONE = GenerateRandomDataUtils.generateRandomNumber(7);
+	public static final String ADDRESS_ID = GenerateRandomDataUtils.generateRandomAlphaNumeric(5);
+	private static final String EMAIL = "aila.bogasieru@ariat.com";
+	private static final String PASSWORD = "Parola12345!";
+
+	@BeforeTest
+	public void setUp() {
+		ChromeDriverManager.getInstance().setup();
+	}
+
+	@Test(priority = 0)
+	public void deleteAddressTest() {
+		logger.info("Starting deleting address test");
+		homePage = new HomePage(new ChromeDriver());
+		homePage.load(environment.DEVELOPMENT.getURL());
+		homePage.closeSubscription();
+		homePage.UKlocation();
+		signInPage = homePage.returnSignInPage();
+		signInPage.returningCustomer(EMAIL);
+		signInPage.returningPassword(PASSWORD);
+		myAccountPage = signInPage.returnMyAccountPage();
+		addressesPage = myAccountPage.returnAddressesPageMiddleNav();
+		addressesPage.deleteAddressCreatedNo("7M4I6");
+		addressesPage.deleteAddressCreated("7M4I6");
+		addressesPage.checkAddress("7M4I6");
+		logoutPage = myAccountPage.returnLogoutFromMyAccountPageTopNav();
+		logoutPage.logout();
+		logger.info("Finishing deleting address test");
+	}
+
+	@Test(priority = 1)
+	public void deleteAddressFromEditTest() {
+		logger.info("Starting deleting address test");
+		homePage = new HomePage(new ChromeDriver());
+		homePage.load(environment.DEVELOPMENT.getURL());
+		homePage.closeSubscription();
+		homePage.UKlocation();
+		signInPage = homePage.returnSignInPage();
+		signInPage.returningCustomer(EMAIL);
+		signInPage.returningPassword(PASSWORD);
+		myAccountPage = signInPage.returnMyAccountPage();
+		addressesPage = myAccountPage.returnAddressesPageMiddleNav();
+		addAddressPage = addressesPage.returnAddressesEdit();
+		addAddressPage.deleteAddress();
+		addressesPage = addAddressPage.returnAddressesFromEditDeletePage();
+		addressesPage.checkAddress("7M4I6");
+		logoutPage = myAccountPage.returnLogoutFromMyAccountPageTopNav();
+		logoutPage.logout();
+		logger.info("Finishing deleting address test");
+	}
+
+	@AfterTest
+	public void tearDown() {
+		homePage.quit();
+		signInPage.quit();
+		myAccountPage.quit();
+		addressesPage.quit();
+		addAddressPage.quit();
+		logoutPage.quit();
+	}
+/*asdjsahdfsakdhkasdkasd*/
+}
