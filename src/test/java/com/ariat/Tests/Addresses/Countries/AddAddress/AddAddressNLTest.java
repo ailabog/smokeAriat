9b1d6f460e0db22229fce854aa995aa8461caab2
Aila Bogasieru <aila.bogasieru@ariat.com>
2019-01-26 22:10:09 +0200

    implement add address for Austria, Blegium, Cehia, China, Italy, Korea, Luxembourg, Netherlands, Norway, Sweden, Turkey and Taiwan

---
+++ src/test/java/com/ariat/Tests/Addresses/Countries/AddAddress/AddAddressNLTest.java
@@ -0,0 +1,89 @@
+package com.ariat.Tests.Addresses.Countries.AddAddress;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.ariat.Enums.EUCountries;
+import com.ariat.Enums.Environments;
+import com.ariat.Pages.HomePagesCountries.HomePage;
+import com.ariat.Pages.HomePagesCountries.HomePageNL;
+import com.ariat.Pages.HomePagesCountries.HomePageUK;
+import com.ariat.Pages.Main.AddAddressesPage;
+import com.ariat.Pages.Main.AddressesPage;
+import com.ariat.Pages.Main.MyAccountPage;
+import com.ariat.Tests.Base.BaseTest;
+import com.ariat.Pages.Header.SignInPage;
+import com.ariat.Utils.GenerateRandomDataUtils;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+
+/**
+ * Adds a new address and checks the address was created for Denmark
+ * 
+ * @author aila.bogasieru@ariat.com
+ *
+ */
+
+public class AddAddressNLTest extends BaseTest {
+
+	private HomePage homePage;
+	private HomePageUK homePageUK;
+	private HomePageNL homePageNL;
+	private SignInPage signInPage;
+	private MyAccountPage myAccountPage;
+	private AddAddressesPage addAddressPage;
+	private AddressesPage addressesPage;
+	private EUCountries euCountry;
+	private Environments environment;
+
+	public static final String ADDRESS = GenerateRandomDataUtils.generateRandomString(5);
+	public static final String CITY = GenerateRandomDataUtils.generateRandomString(5);
+	public static final String POST_CODE = GenerateRandomDataUtils.generateRandomNumber(5);
+	public static final String PHONE = GenerateRandomDataUtils.generateRandomNumber(7);
+	public static final String ADDRESS_ID = GenerateRandomDataUtils.generateRandomAlphaNumeric(5);
+	private static final String EMAIL = "aila.bogasieru@ariat.com";
+	private static final String PASSWORD = "Parola12345!";
+
+	@BeforeTest
+	public void setUp() {
+		ChromeDriverManager.getInstance().setup();
+	}
+
+	@Test
+	public void addAddressNLTest() {
+		logger.info("Starting add address Netherlands test");
+		homePage = new HomePage(new ChromeDriver());
+		homePage.load(environment.DEVELOPMENT.getURL());
+		homePageUK = (HomePageUK) homePage.chooseEULocation(euCountry.UK, euCountry.UK.getCurrencyISO());
+		homePageNL = (HomePageNL) homePage.chooseEULocation(euCountry.NL, euCountry.NL.getCurrencyISO());
+		signInPage = homePageNL.returnSignInPage();
+		signInPage.returningCustomer(EMAIL, "EnglishUK");
+		signInPage.returningPassword(PASSWORD);
+		myAccountPage = signInPage.returnMyAccountPage();
+		addAddressPage = myAccountPage.returnAddAddressesPageMiddleNav();
+		addAddressPage.enterFName("A");
+		addAddressPage.enterLName("B");
+		addAddressPage.enterAddress1("Basarabia Blvd, No 62");
+		addAddressPage.enterCity(CITY);
+		addAddressPage.selectCountry("Netherlands");
+		addAddressPage.enterPostCode(POST_CODE);
+		addAddressPage.enterPhone(PHONE);
+		addAddressPage.enterAddressId(ADDRESS_ID);
+		addressesPage = addAddressPage.returnAddressesPage();
+		//addressesPage.checkAddress(ADDRESS_ID);
+		logger.info("Finishing add address Netherlands test");
+	}
+	
+	@AfterTest
+	public void tearDown() {
+		homePage.quit();
+		homePageUK.quit();
+		homePageNL.quit();
+		signInPage.quit();
+		myAccountPage.quit();
+		addAddressPage.quit();
+		addressesPage.quit();
+	}
+}
---
+++ src/test/java/com/ariat/Tests/Addresses/Countries/AddAddress/AddAddressNOTest.java
@@ -0,0 +1,89 @@
+package com.ariat.Tests.Addresses.Countries.AddAddress;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.ariat.Enums.EUCountries;
+import com.ariat.Enums.Environments;
+import com.ariat.Pages.HomePagesCountries.HomePage;
+import com.ariat.Pages.HomePagesCountries.HomePageNO;
+import com.ariat.Pages.HomePagesCountries.HomePageUK;
+import com.ariat.Pages.Main.AddAddressesPage;
+import com.ariat.Pages.Main.AddressesPage;
+import com.ariat.Pages.Main.MyAccountPage;
+import com.ariat.Tests.Base.BaseTest;
+import com.ariat.Pages.Header.SignInPage;
+import com.ariat.Utils.GenerateRandomDataUtils;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+
+/**
+ * Adds a new address and checks the address was created for Denmark
+ * 
+ * @author aila.bogasieru@ariat.com
+ *
+ */
+
+public class AddAddressNOTest extends BaseTest {
+
+	private HomePage homePage;
+	private HomePageUK homePageUK;
+	private HomePageNO homePageNO;
+	private SignInPage signInPage;
+	private MyAccountPage myAccountPage;
+	private AddAddressesPage addAddressPage;
+	private AddressesPage addressesPage;
+	private EUCountries euCountry;
+	private Environments environment;
+
+	public static final String ADDRESS = GenerateRandomDataUtils.generateRandomString(5);
+	public static final String CITY = GenerateRandomDataUtils.generateRandomString(5);
+	public static final String POST_CODE = GenerateRandomDataUtils.generateRandomNumber(5);
+	public static final String PHONE = GenerateRandomDataUtils.generateRandomNumber(7);
+	public static final String ADDRESS_ID = GenerateRandomDataUtils.generateRandomAlphaNumeric(5);
+	private static final String EMAIL = "aila.bogasieru@ariat.com";
+	private static final String PASSWORD = "Parola12345!";
+
+	@BeforeTest
+	public void setUp() {
+		ChromeDriverManager.getInstance().setup();
+	}
+
+	@Test
+	public void addAddressNOTest() {
+		logger.info("Starting add address Norway test");
+		homePage = new HomePage(new ChromeDriver());
+		homePage.load(environment.DEVELOPMENT.getURL());
+		homePageUK = (HomePageUK) homePage.chooseEULocation(euCountry.UK, euCountry.UK.getCurrencyISO());
+		homePageNO = (HomePageNO) homePage.chooseEULocation(euCountry.NO, euCountry.NO.getCurrencyISO());
+		signInPage = homePageNO.returnSignInPage();
+		signInPage.returningCustomer(EMAIL, "EnglishUK");
+		signInPage.returningPassword(PASSWORD);
+		myAccountPage = signInPage.returnMyAccountPage();
+		addAddressPage = myAccountPage.returnAddAddressesPageMiddleNav();
+		addAddressPage.enterFName("A");
+		addAddressPage.enterLName("B");
+		addAddressPage.enterAddress1("Basarabia Blvd, No 62");
+		addAddressPage.enterCity(CITY);
+		addAddressPage.selectCountry("Norway");
+		addAddressPage.enterPostCode(POST_CODE);
+		addAddressPage.enterPhone(PHONE);
+		addAddressPage.enterAddressId(ADDRESS_ID);
+		addressesPage = addAddressPage.returnAddressesPage();
+		//addressesPage.checkAddress(ADDRESS_ID);
+		logger.info("Finishing add address Norway test");
+	}
+	
+	@AfterTest
+	public void tearDown() {
+		homePage.quit();
+		homePageUK.quit();
+		homePageNO.quit();
+		signInPage.quit();
+		myAccountPage.quit();
+		addAddressPage.quit();
+		addressesPage.quit();
+	}
+}
