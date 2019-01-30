b3a2a69a9843ced61fce124a78b3fa3d3efb9ad5
Aila Bogasieru <aila.bogasieru@ariat.com>
2019-01-30 23:54:35 +0200

    add missing tests in xml files for new added countries: Austria, Cehia, Belgium, Italy, Netherlands, Norway, Luxembourg, Sweden, Jersey, China, Turkey, Taiwan & Korea

---
+++ src/test/java/com/ariat/Tests/Addresses/Countries/MakeDefaultAddress/MakeDefaultAddressCNTest.java
@@ -0,0 +1,74 @@
+package com.ariat.Tests.Addresses.Countries.MakeDefaultAddress;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.ariat.Enums.GlobalCountries;
+import com.ariat.Enums.Environments;
+import com.ariat.Pages.HomePagesCountries.HomePage;
+import com.ariat.Pages.HomePagesCountries.HomePageCN;
+import com.ariat.Pages.Main.AddressesPage;
+import com.ariat.Pages.Main.MyAccountPage;
+import com.ariat.Tests.Base.BaseTest;
+import com.ariat.Pages.Header.SignInPage;
+import com.ariat.Utils.GenerateRandomDataUtils;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+
+/**
+ * Make default test Japan
+ * 
+ * @author aila.bogasieru@ariat.com
+ *
+ */
+
+public class MakeDefaultAddressCNTest extends BaseTest {
+
+	private HomePage homePage;
+	private HomePageCN homePageCN;
+   private SignInPage signInPage;
+	private MyAccountPage myAccountPage;
+	private AddressesPage addressesPage;
+	private Environments environment;
+	private GlobalCountries country;
+
+	public static final String ADDRESS = GenerateRandomDataUtils.generateRandomString(5);
+	public static final String CITY = GenerateRandomDataUtils.generateRandomString(5);
+	public static final String POST_CODE = GenerateRandomDataUtils.generateRandomNumber(5);
+	public static final String PHONE = GenerateRandomDataUtils.generateRandomNumber(7);
+	public static final String ADDRESS_ID = GenerateRandomDataUtils.generateRandomAlphaNumeric(5);
+	private static final String EMAIL = "aila.bogasieru@yahoo.com";
+	private static final String PASSWORD = "Parola12345!";
+
+	@BeforeTest
+	public void setUp() {
+		ChromeDriverManager.getInstance().setup();
+	}
+
+	@Test
+	public void makeDefaultAddressJPTest() {
+		logger.info("Starting make default address Japan test");
+		homePage = new HomePage(new ChromeDriver());
+		homePage.load(environment.DEVELOPMENT.getURL());
+		homePageCN = (HomePageCN) homePage.chooseGlobalLocation(country.CN, country.CN.getCurrencyISO());
+		signInPage = homePageCN.returnSignInPage();
+		signInPage.returningCustomer(EMAIL, "EnglishUS");
+		signInPage.returningPassword(PASSWORD);
+		myAccountPage = signInPage.returnMyAccountPage();
+		addressesPage= myAccountPage.returnAddressesPageMiddleNav();
+		addressesPage.makeDefaultAddressCreated("235432");
+		addressesPage.assertMakeDefault("DEFAULT | 235432");
+		logger.info("Finishing make default address Japan test");
+	}
+	
+	@AfterTest
+	public void tearDown() {
+		homePage.quit();
+		homePageCN.quit();
+		signInPage.quit();
+		myAccountPage.quit();
+		addressesPage.quit();
+	}
+}
