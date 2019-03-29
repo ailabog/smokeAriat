f06fc6c170f8faa7ee7455bab42f68a8c296b240
Aila Bogasieru <aila.bogasieru@ariat.com>
2019-03-29 16:34:49 +0200

    implement automated tests for negative add credit card for CN, CH, AT, IT, JE, US, RU, SE, KR, TW, BE

---
+++ src/test/java/com/ariat/Tests/CreditCard/Countries/NegativeCreditCard/NegativeAddCreditCardKRTest.java
@@ -0,0 +1,160 @@
+package com.ariat.Tests.CreditCard.Countries.NegativeCreditCard;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.ariat.Enums.GlobalCountries;
+import com.ariat.Enums.Environments;
+import com.ariat.Enums.ListOfCreditCards;
+import com.ariat.Pages.HomePagesCountries.HomePage;
+import com.ariat.Pages.HomePagesCountries.HomePageKR;
+import com.ariat.Pages.Main.AddACreditCardPage;
+import com.ariat.Pages.Main.MyAccountPage;
+import com.ariat.Pages.Main.PaymentInformationPage;
+import com.ariat.Tests.Base.BaseTest;
+import com.ariat.Pages.Header.SignInPage;
+
+/**
+ * Tests for Negative Add Credit card Korea
+ * 
+ * @author aila.bogasieru@ariat.com
+ *
+ */
+
+public class NegativeAddCreditCardKRTest extends BaseTest {
+
+	private HomePage homePage;
+	private HomePageKR homePageKR;
+	private SignInPage signInPage;
+	private MyAccountPage myAccountPage;
+	private AddACreditCardPage addACreditCardPage;
+	private PaymentInformationPage paymentInfoPage;
+	private Environments environment;
+	private GlobalCountries country;
+	private ListOfCreditCards typeCard;
+
+	private static final String EMAIL = "aila.bogasieru@ariat.com";
+	private static final String PASSWORD = "Parola12345!";
+	private static final String CARD_ID = "ryttrytry";
+	private static final String CARD_OWNER = "5654657665";
+	private static final String YEAR = "2023";
+	private static final String MONTH = "January";
+
+	private static final String YEAR1 = "2009";
+	private static final String MONTH1 = "January";
+
+	private static final String CARD_IDWILD = "#$%^&*(";
+	private static final String CARD_OWNERWILD = "@#$%^&*(";
+
+	public static final String filePath = "\\Users\\Aila\\eclipse-workspace\\ariat-regression\\src\\test\\resources\\chromedriver\\chromedriver.exe";
+
+	@BeforeTest
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver", filePath);
+	}
+
+	@Test(priority = 0)
+	public void negativeAddCreditCardUKTest() {
+		String expirationDate = "MONTH/YEAR";
+		logger.info("Starting add negative credit card UK test");
+		homePage = new HomePage(new ChromeDriver());
+		homePage.load(environment.DEVELOPMENT.getURL());
+		homePageKR = (HomePageKR) homePage.chooseGlobalLocation(country.KR, country.KR.getCurrencyISO());
+		signInPage = homePageKR.returnSignInPage();
+		signInPage.returningCustomer(EMAIL, "EnglishUK");
+		signInPage.returningPassword(PASSWORD);
+		myAccountPage = signInPage.returnMyAccountPage();
+		addACreditCardPage = myAccountPage.returnAddACreditCardMiddleNav();
+		addACreditCardPage.enterCardId(CARD_ID);
+		addACreditCardPage.enterCardOwner(CARD_OWNER);
+		addACreditCardPage.selectTypeCard(typeCard.VISA.getName());
+		addACreditCardPage.enterCardNo("dghdfjghdfkghdf");
+		addACreditCardPage.enterSecurityCode(typeCard.VISA.getCvs());
+		addACreditCardPage.selectExpirationYearCard(YEAR);
+		addACreditCardPage.selectExpirationMonthCard(MONTH);
+		paymentInfoPage = addACreditCardPage.returnPaymentInformationPage();
+		paymentInfoPage.checkCreditCard(CARD_OWNER, typeCard.VISA.getName(), expirationDate);
+		logger.info("Finishing add negative credit card UK test");
+	}
+
+	@Test(priority = 1)
+	public void negativeAddCreditCardUKTestWildCard() {
+		String expirationDate = "MONTH/YEAR";
+		logger.info("Starting add negative credit card UK test");
+		homePage = new HomePage(new ChromeDriver());
+		homePage.load(environment.DEVELOPMENT.getURL());
+		homePageKR = (HomePageKR) homePage.chooseGlobalLocation(country.KR, country.KR.getCurrencyISO());
+		signInPage = homePageKR.returnSignInPage();
+		signInPage.returningCustomer(EMAIL, "EnglishUK");
+		signInPage.returningPassword(PASSWORD);
+		myAccountPage = signInPage.returnMyAccountPage();
+		addACreditCardPage = myAccountPage.returnAddACreditCardMiddleNav();
+		addACreditCardPage.enterCardId(CARD_IDWILD);
+		addACreditCardPage.enterCardOwner(CARD_OWNERWILD);
+		addACreditCardPage.selectTypeCard(typeCard.VISA.getName());
+		addACreditCardPage.enterCardNo("@#$%^&*(");
+		addACreditCardPage.enterSecurityCode(typeCard.VISA.getCvs());
+		addACreditCardPage.selectExpirationYearCard(YEAR);
+		addACreditCardPage.selectExpirationMonthCard(MONTH);
+		paymentInfoPage = addACreditCardPage.returnPaymentInformationPage();
+		paymentInfoPage.checkCreditCard(CARD_OWNER, typeCard.VISA.getName(), expirationDate);
+		logger.info("Finishing add negative credit card UK test");
+	}
+
+	@Test(priority = 2)
+	public void negativeAddCreditCardUKTestMissingValues() {
+		String expirationDate = "MONTH/YEAR";
+		logger.info("Starting add negative credit card UK test");
+		homePage = new HomePage(new ChromeDriver());
+		homePage.load(environment.DEVELOPMENT.getURL());
+		homePageKR = (HomePageKR) homePage.chooseGlobalLocation(country.KR, country.KR.getCurrencyISO());
+		signInPage = homePageKR.returnSignInPage();
+		signInPage.returningCustomer(EMAIL, "EnglishUK");
+		signInPage.returningPassword(PASSWORD);
+		myAccountPage = signInPage.returnMyAccountPage();
+		addACreditCardPage = myAccountPage.returnAddACreditCardMiddleNav();
+		addACreditCardPage.selectTypeCard(typeCard.VISA.getName());
+		addACreditCardPage.enterSecurityCode(typeCard.VISA.getCvs());
+		addACreditCardPage.selectExpirationYearCard(YEAR);
+		addACreditCardPage.selectExpirationMonthCard(MONTH);
+		paymentInfoPage = addACreditCardPage.returnPaymentInformationPage();
+		paymentInfoPage.checkCreditCard(CARD_OWNER, typeCard.VISA.getName(), expirationDate);
+		logger.info("Finishing add negative credit card UK test");
+	}
+
+	@Test(priority = 3)
+	public void negativeAddCreditCardUKTestExpirationDate() {
+		String expirationDate = "MONTH1/YEAR1";
+		logger.info("Starting add a credit card UK test");
+		homePage = new HomePage(new ChromeDriver());
+		homePage.load(environment.DEVELOPMENT.getURL());
+		homePageKR = (HomePageKR) homePage.chooseGlobalLocation(country.KR, country.KR.getCurrencyISO());
+		signInPage = homePageKR.returnSignInPage();
+		signInPage.returningCustomer(EMAIL, "EnglishUK");
+		signInPage.returningPassword(PASSWORD);
+		myAccountPage = signInPage.returnMyAccountPage();
+		addACreditCardPage = myAccountPage.returnAddACreditCardMiddleNav();
+		addACreditCardPage.enterCardId(CARD_ID);
+		addACreditCardPage.enterCardOwner(CARD_OWNER);
+		addACreditCardPage.selectTypeCard(typeCard.VISA.getName());
+		addACreditCardPage.enterCardNo(typeCard.VISA.getNumber());
+		addACreditCardPage.enterSecurityCode(typeCard.VISA.getCvs());
+		addACreditCardPage.selectExpirationYearCard(YEAR1);
+		addACreditCardPage.selectExpirationMonthCard(MONTH1);
+		paymentInfoPage = addACreditCardPage.returnPaymentInformationPage();
+		paymentInfoPage.checkCreditCard(CARD_OWNER, typeCard.VISA.getName(), expirationDate);
+		logger.info("Finishing add a credit card UK test");
+	}
+
+	@AfterTest
+	public void tearDown() {
+		homePage.quit();
+		homePageKR.quit();
+		signInPage.quit();
+		addACreditCardPage.quit();
+		paymentInfoPage.quit();
+		myAccountPage.quit();
+	}
+}
