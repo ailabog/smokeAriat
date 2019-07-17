d1ab7bccf9e41bac39c641bf5ae9b2101ef98ccd
Aila Bogasieru <aila.bogasieru@ariat.com>
2019-07-18 00:05:23 +0300

    set relative path for Jenkins server

--- src/test/java/com/ariat/Tests/Account/Countries/CreateAccount/CreateAccountUKTest.java
+++ src/test/java/com/ariat/Tests/Account/Countries/CreateAccount/CreateAccountUKTest.java
@@ -50,11 +50,11 @@ public class CreateAccountUKTest extends BaseTest {
 	public static final String ERROR_MESSAGE = "Sorry this order number or postcode does not match our records. Check your records and try again.";
 	public static final String RELATIV_PATH = "/usr/local/bin/chromedriver";
     
-    public static final String ABSOLUTE_PATH = System.getProperty("user.dir")+ RELATIV_PATH;
+    //public static final String ABSOLUTE_PATH = System.getProperty("user.dir")+ RELATIV_PATH;
     
 	@BeforeTest
 	public void setUp() {
-		System.setProperty("webdriver.chrome.driver", ABSOLUTE_PATH);
+		System.setProperty("webdriver.chrome.driver", RELATIV_PATH);
 	}
     
 /*	@BeforeTest
