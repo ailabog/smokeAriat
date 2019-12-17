9b40c4dbeaa30a8116329a2b157be464a1c3007e
Aila Bogasieru <aila.bogasieru@ariat.com>
2019-12-17 22:54:04 +0200

    add US tests

--- src/main/java/com/ariat/Pages/HomePagesCountries/HomePage.java
+++ src/main/java/com/ariat/Pages/HomePagesCountries/HomePage.java
@@ -114,22 +114,24 @@ public class HomePage extends BasePage implements List<HomePage> {
 			}
 			return new HomePageUS(driver);
 
-			//local version
+			//Jenkins version 
 		case "(United Kingdom)":
-			logger.info("I choose English United Kingdom as location");
-			WebDriverUtils.clickOnElementWithWait(driver, chooseLocationArrow);
+		logger.info("I choose English United Kingdom as location");
+		WebDriverUtils.clickOnElementWithWait(driver, chooseLocationArrow);
+		WebDriverUtils.explicitWait(driver, WebDriverUtils.WAIT_6000_SECONDS);
+		if (WebDriverUtils.findElement(driver, countrySelectorWindow) != null) {
+			WebDriverUtils.clickOnElementWithWait(driver, listCountries);
 			WebDriverUtils.explicitWait(driver, WebDriverUtils.WAIT_6000_SECONDS);
-			if (WebDriverUtils.findElement(driver, countrySelectorWindow) != null) {
-				WebDriverUtils.clickOnElementWithWait(driver, listCountries);
-				WebDriverUtils.explicitWait(driver, WebDriverUtils.WAIT_6000_SECONDS);
-				WebDriverUtils.scrolltoElement(driver, euCountry.UK.getLocator());
-				WebDriverUtils.explicitWait(driver, WebDriverUtils.WAIT_6000_SECONDS);
-				logger.info("Saving location...");
-				WebDriverUtils.clickOnElementWithWait(driver, saveAndContinueLocationButton);
-				WebDriverUtils.explicitWait(driver, WebDriverUtils.WAIT_6000_SECONDS);
-				WebDriverUtils.clickOnElementWithWait(driver, euCountry.UK.getLocator());
-			}
-			return new HomePageUK(driver);
+			WebDriverUtils.scrollLittDown(driver, euCountry.UK.getLocator());
+			WebDriverUtils.clickOnElementWithWait(driver, euCountry.UK.getLocator());
+			WebDriverUtils.explicitWait(driver, WebDriverUtils.WAIT_6000_SECONDS);
+			logger.info("Saving location...");
+			WebDriverUtils.clickOnElementWithWait(driver, saveAndContinueLocationButton);
+			WebDriverUtils.explicitWait(driver, WebDriverUtils.WAIT_6000_SECONDS);
+			WebDriverUtils.clickOnElementWithWait(driver, euCountry.UK.getLocator());
+			WebDriverUtils.clickOnElementWithWait(driver, saveAndContinueLocationButton);
+		}
+		return new HomePageUK(driver);
 			
 		default:
 			throw new RuntimeException("Country" + euCountry + "not supported");
