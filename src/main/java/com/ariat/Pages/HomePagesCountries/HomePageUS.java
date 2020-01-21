75a9046a5324edd2166807a3b92268f28e68bcdb
Aila Bogasieru <aila.bogasieru@ariat.com>
2020-01-21 19:58:26 +0200

    change product

--- src/main/java/com/ariat/Pages/HomePagesCountries/HomePageUS.java
+++ src/main/java/com/ariat/Pages/HomePagesCountries/HomePageUS.java
@@ -108,7 +108,7 @@ public class HomePageUS extends BasePage implements List<HomePage> {
 	private By topsAndTshirtsText = By.xpath("//*[@id=\"main\"]/div/div[2]/div[2]/div[1]/span[1]");
 
 	//private By garmentBagProduct = By.xpath("//img[contains(@title, 'Ariat Team Garment Bag')]");
-	private By garmentBagProduct = By.cssSelector("#ce3268bb64290376d227adfa41 .psd-4-full-width-video-module-1__image");
+	private By garmentBagProduct = By.cssSelector("#\\\\33 c79299b2a77de93250532b7fd .psd-4-full-width-video-module-1__image");
 	//css=#ce3268bb64290376d227adfa41 .psd-4-full-width-video-module-1__image 
 	//xpath=//img[@alt='Ariat Team Garment Bag']
 	private By garmentText = By.xpath("//*contains[text()='Garment Bag']");
